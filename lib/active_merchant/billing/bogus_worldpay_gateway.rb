6f12f30402d7ad9a4c4abb0ab3cdcd1d1810b1f1
Jeff Yucis <jyucis@workarea.com>
2019-11-22 15:42:56 -0500

    Fix rubocop in bogus gateway
    no changelog

--- lib/active_merchant/billing/bogus_worldpay_gateway.rb
+++ lib/active_merchant/billing/bogus_worldpay_gateway.rb
@@ -1,23 +1,23 @@
 module ActiveMerchant
   module Billing
     class BogusWorldpayGateway < BogusGateway
-      def create_token(paysource, options= {})
+      def create_token(paysource, options = {})
         case normalize(paysource)
-          when /1$/
+        when /1$/
             Response.new(
               true,
               SUCCESS_MESSAGE,
               { payment_token_id: SecureRandom.hex(10).upcase + '1' },
-              :test => true,
-              :authorization => AUTHORIZATION
+              test: true,
+              authorization: AUTHORIZATION
             )
-          when /2$/
+        when /2$/
             Response.new(
               false,
               FAILURE_MESSAGE,
               { payment_token_id: nil, error: FAILURE_MESSAGE },
-              :test => true,
-              :error_code => STANDARD_ERROR_CODE[:processing_error]
+              test: true,
+              error_code: STANDARD_ERROR_CODE[:processing_error]
             )
           else
             raise Error, error_message(paysource)
@@ -27,39 +27,38 @@ module ActiveMerchant
       def authorize_swipe(money, paysource, options = {})
         money = amount(money)
         case normalize(paysource)
-          when /2$/
-            Response.new(false, FAILURE_MESSAGE, {:authorized_amount => money, :error => FAILURE_MESSAGE }, :test => true, :error_code => STANDARD_ERROR_CODE[:processing_error])
-          when /3$/
-            Response.new(false, FAILURE_MESSAGE, {:authorized_amount => money, :error => FAILURE_MESSAGE }, :test => true, :error_code => STANDARD_ERROR_CODE[:processing_error])
+        when /2$/
+            Response.new(false, FAILURE_MESSAGE, { authorized_amount: money, error: FAILURE_MESSAGE }, test: true, error_code: STANDARD_ERROR_CODE[:processing_error])
+        when /3$/
+            Response.new(false, FAILURE_MESSAGE, { authorized_amount: money, error: FAILURE_MESSAGE }, test: true, error_code: STANDARD_ERROR_CODE[:processing_error])
           else
-            Response.new(true, SUCCESS_MESSAGE, {:authorized_amount => money}, :test => true, :authorization => AUTHORIZATION )
+            Response.new(true, SUCCESS_MESSAGE, { authorized_amount: money }, test: true, authorization: AUTHORIZATION)
         end
       end
 
       def purchase_swipe(money, paysource, options = {})
         money = amount(money)
         case normalize(paysource)
-          when /3$/
+        when /3$/
             raise Error, error_message(paysource)
-          when /2$/
+        when /2$/
             Response.new(
               false,
               FAILURE_MESSAGE,
-              {:paid_amount => money, :error => FAILURE_MESSAGE },
-              :test => true,
-              :error_code => STANDARD_ERROR_CODE[:processing_error]
+              { paid_amount: money, error: FAILURE_MESSAGE },
+              test: true,
+              error_code: STANDARD_ERROR_CODE[:processing_error]
             )
           else
             Response.new(
               true,
               SUCCESS_MESSAGE,
-              {:paid_amount => money},
-              :test => true,
-              :authorization => AUTHORIZATION
+              { paid_amount: money },
+              test: true,
+              authorization: AUTHORIZATION
             )
         end
       end
     end
   end
 end
-
